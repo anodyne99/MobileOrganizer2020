75db6f21af01a48c94f4f1f06c22d42cdbae3999
Eric .O <ericosuala@gmail.con>
2020-11-23 20:09:23 -0600

    Adding Home Activity
    Home activity to enter user details

---
+++ Final Project/Source/EventPlanner/app/src/main/java/com/example/eventplanner/HomeActivity.java
@@ -0,0 +1,41 @@
+package com.example.eventplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+
+    public void redirectToMainActivity (View v) {
+        Intent redirect = new Intent(HomeActivity.this, MainActivity.class);
+        startActivity(redirect);
+
+    }
+
+
+
+}
--- Final Project/Source/EventPlanner/app/src/main/java/com/example/eventplanner/LoginActivity.java
+++ Final Project/Source/EventPlanner/app/src/main/java/com/example/eventplanner/LoginActivity.java
@@ -15,14 +15,16 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button loginBtn;
-    Button registerBtn;
+    Button loginBtn,registerBtn,lunch,update,save;
+    EditText name,email,password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +32,19 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         loginBtn=(Button)findViewById(R.id.loginBtn);
+        lunch=(Button)findViewById(R.id.lunch);
+        update=(Button)findViewById(R.id.update);
+        save=(Button)findViewById(R.id.save);
+        registerBtn = (Button) findViewById(R.id.registerBtn);
+        name = (EditText) findViewById(R.id.name);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+
     }
 
 
-    public void redirectToMainActivity (View v) {
-        Intent redirect = new Intent(LoginActivity.this, MainActivity.class);
+    public void redirectToHomeActivity (View v) {
+        Intent redirect = new Intent(LoginActivity.this, HomeActivity.class);
         startActivity(redirect);
 
     }
