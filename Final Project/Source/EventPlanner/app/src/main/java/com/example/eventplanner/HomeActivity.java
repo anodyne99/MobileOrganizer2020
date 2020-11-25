129218522307dd4b29c17d4eb959a9f0c5b25738
Eric .O <ericosuala@gmail.con>
2020-11-25 00:09:15 -0600

    save button to save user data and show to display it

--- Final Project/Source/EventPlanner/app/src/main/java/com/example/eventplanner/HomeActivity.java
+++ Final Project/Source/EventPlanner/app/src/main/java/com/example/eventplanner/HomeActivity.java
@@ -10,6 +10,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,11 +21,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class HomeActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button loginBtn, registerBtn, lunch, show, save;
+    EditText name, age, gender, location, email, password;
+    User user;
+
+    //FirebaseDatabase rootNode;
+    DatabaseReference reff;
 
 
     @Override
@@ -31,25 +48,63 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        lunch = (Button) findViewById(R.id.launch);
+         show = (Button) findViewById(R.id.show);
+        save = (Button) findViewById(R.id.save);
+        registerBtn = (Button) findViewById(R.id.registerBtn);
+        name = (EditText) findViewById(R.id.name);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        age = (EditText) findViewById(R.id.age);
+        gender = (EditText) findViewById(R.id.gender);
+        location = (EditText) findViewById(R.id.location);
+        textView= (TextView) findViewById(R.id.textView) ;
+
+        user= new User();
+        reff = FirebaseDatabase.getInstance().getReference().child("users");
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String na = name.getText().toString().trim();
+                String ag = age.getText().toString().trim();
+                String gen = gender.getText().toString().trim();
+                String loc = location.getText().toString().trim();
+
+                user.setName(name.getText().toString().trim());
+                user.setAge(ag);
+                user.setGender(gen);
+                user.setLocation(loc);
+                reff.push().setValue(user);
+                Toast.makeText(HomeActivity.this,"Data inserted successfully",Toast.LENGTH_LONG).show();
+
+                textView.setText("Name: " + na + " Age: " + ag  + " Gen: " + gen + " Loc: " + loc);
+
+
+            }
+        });
+
+
+     }
+
+
+
+        public void redirectToMainActivity (View v){
+            Intent redirect = new Intent(HomeActivity.this, MainActivity.class);
+            startActivity(redirect);
+
+        }
+
+
+        //Redirecting to LoginActivity
+        public void redirectToLoginActivity (View v){
+            FirebaseAuth mAuth = FirebaseAuth.getInstance();
+            mAuth.getInstance().signOut();
+
+            Intent redirect = new Intent(HomeActivity.this, LoginActivity.class);
+            startActivity(redirect);
+
+
+        }
 
     }
-
-    public void redirectToMainActivity (View v) {
-        Intent redirect = new Intent(HomeActivity.this, MainActivity.class);
-        startActivity(redirect);
-
-    }
-
-
-    //Redirecting to LoginActivity
-    public void redirectToLoginActivity (View v) {
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        mAuth.getInstance().signOut();
-
-        Intent redirect = new Intent(HomeActivity.this, LoginActivity.class);
-        startActivity(redirect);
-
-
-    }
-
-}
--- Final Project/Source/EventPlanner/app/src/main/java/com/example/eventplanner/LoginActivity.java
+++ Final Project/Source/EventPlanner/app/src/main/java/com/example/eventplanner/LoginActivity.java
@@ -37,17 +37,15 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginBtn=(Button)findViewById(R.id.loginBtn);
-        lunch=(Button)findViewById(R.id.launch);
-        update=(Button)findViewById(R.id.update);
-        save=(Button)findViewById(R.id.save);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        lunch = (Button) findViewById(R.id.launch);
         registerBtn = (Button) findViewById(R.id.registerBtn);
-        name = (EditText) findViewById(R.id.name);
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
 
@@ -55,8 +53,8 @@ public class LoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         //checking if user has already been created before or already logged in
-        if(mAuth.getCurrentUser() != null){
-            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
 
         }
 
@@ -67,42 +65,72 @@ public class LoginActivity extends AppCompatActivity {
                 String mEmail = email.getText().toString().trim();
                 String mPassword = password.getText().toString().trim();
 
-                if (TextUtils.isEmpty(mEmail)){
+                if (TextUtils.isEmpty(mEmail)) {
                     email.setError("Email is required");
                     return;
                 }
 
-                if(TextUtils.isEmpty(mPassword)) {
+                if (TextUtils.isEmpty(mPassword)) {
                     password.setError("Password is required.");
                     return;
 
                 }
 
-                if(password.length() < 6){
+                if (password.length() < 6) {
                     password.setError("Password must be >= 6 characters");
                     return;
                 }
-              mAuth.createUserWithEmailAndPassword(mEmail,mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                  @Override
-                  public void onComplete(@NonNull Task<AuthResult> task) {
-                      if(task.isSuccessful()){
-                          Toast.makeText(LoginActivity.this,"User Created",Toast.LENGTH_SHORT).show();
-                          startActivity(new Intent(getApplicationContext(),HomeActivity.class));
-                      }
-
-                      else{
-                        Toast.makeText(LoginActivity.this,"User not validated" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-                      }
-                  }
-              });
+                mAuth.createUserWithEmailAndPassword(mEmail, mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        } else {
+                            Toast.makeText(LoginActivity.this, "User not validated" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
             }
         });
+    }
+
+    public void redirectToHome(View v) {
 
 
+        // Store values at the time of the login attempt.
+        String mEmail = email.getText().toString().trim();
+        String mPassword = password.getText().toString().trim();
 
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(mPassword) ) {
+            password.setError("Password is required.");
 
+        }
 
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(mEmail)) {
+            email.setError("Email is required");
+        }
+
+        //authenticate user
+        mAuth.signInWithEmailAndPassword(mEmail, mPassword)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (!task.isSuccessful()) {
+                            // there was an error
+                            Toast.makeText(LoginActivity.this, "Authntication Failed", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
     }
 
 
