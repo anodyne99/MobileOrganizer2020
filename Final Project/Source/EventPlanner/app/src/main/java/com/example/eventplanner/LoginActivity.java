112e2e2b9f1bdca1fa448480a4826a9ba785d7ce
anodyne99 <60171134+anodyne99@users.noreply.github.com>
2020-12-13 14:40:57 -0600

    Updated push test

--- Final Project/Source/EventPlanner/app/src/main/java/com/example/eventplanner/LoginActivity.java
+++ Final Project/Source/EventPlanner/app/src/main/java/com/example/eventplanner/LoginActivity.java
@@ -5,29 +5,17 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
